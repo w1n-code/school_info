9cc03b576eda52d29079f2f1391ec35f60649fe4
w1n-code <131504642+w1n-code@users.noreply.github.com>
2023-05-28 01:01:54 +0900

    Update test.js

--- test.js
+++ test.js
@@ -8,30 +8,45 @@ drawerToggle.addEventListener('click', function() {
 
 
 //아래는 kakaomap
-var container = document.getElementById('map');
-var options = {
-  center: new kakao.maps.LatLng(35.88720165868838, 128.58509478903335),
-  level: 8
-};
-var map = new kakao.maps.Map(container, options);
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+    mapOption = { 
+        center: new kakao.maps.LatLng(35.88389261025553, 128.58642961643818),
+     	level:5
+    };
 
-var markerPositions = [
-  new kakao.maps.LatLng(35.88720165868838, 128.58509478903335), // Blue marker position
-  new kakao.maps.LatLng(35.88148754395694, 128.57626086398895), // Yellow marker position 1
-  new kakao.maps.LatLng(35.88082239685244, 128.59862890053688), // Yellow marker position 2
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+ 
+// 마커를 표시할 위치와 title 객체 배열입니다 
+var positions = [
+  {
+        title: '대구일중학교', 
+        latlng: new kakao.maps.LatLng(35.88720165868838, 128.58509478903335)
+    },
+  {
+        title: '경일중학교', 
+        latlng: new kakao.maps.LatLng(35.88148754395694, 128.57626086398895)
+    },
+  {
+        title: '경명여자중학교', 
+        latlng: new kakao.maps.LatLng(35.88082239685244, 128.59862890053688)
+    },
 ];
 
-for (var i = 0; i < markerPositions.length; i++) {
-  var markerOptions = {
-    position: markerPositions[i],
-    map: map,
-    image: {
-      src: (i === 0) ? 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-3875d7.png' : 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-fdff5e.png',
-      size: new kakao.maps.Size(40, 40),
-      origin: new kakao.maps.Point(0, 0),
-      anchor: new kakao.maps.Point(20, 40)
-    }
-  };
+var imageSrc = (i === 0) ? "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png" : "https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-3875d7.png";
 
-  var marker = new kakao.maps.Marker(markerOptions);
+for (var i = 0; i < positions.length; i ++) {
+    
+    // 마커 이미지의 이미지 크기 입니다
+    var imageSize = new kakao.maps.Size(24, 35); 
+    
+    // 마커 이미지를 생성합니다    
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+    
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage
+    });
 }
