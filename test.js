095d9e755ee19a1d933187f0175ab00dbbbdb0e8
w1n-code <131504642+w1n-code@users.noreply.github.com>
2023-05-28 00:05:06 +0900

    Update test.js
    kakaomap, add marker

--- test.js
+++ test.js
@@ -1,15 +1,37 @@
-const drawerMenu = document.querySelector('.drawer-menu'); //querySelectorAll함수를 이용해 div안의 class 값 'drawer-menu' 값을 가져옴
+const drawerMenu = document.querySelector('.drawer-menu');
 const drawerToggle = document.querySelector('.drawer-toggle');
 
-drawerToggle.addEventListener('click', function() { //addEventListener를 지정해 클릭시 function함수를 실행
+drawerToggle.addEventListener('click', function() {
   console.log(drawerMenu)
-  drawerMenu.classList.toggle('open'); //classList.toggle: 기존 값이 ' '이었다면 'open'로 바뀌고 기존 값이 'open'이었다면, ' '으로 바뀜
+  drawerMenu.classList.toggle('open');
 });
 
+
+//아래는 kakaomap
 var container = document.getElementById('map');
 var options = {
   center: new kakao.maps.LatLng(35.88720165868838, 128.58509478903335),
   level: 3
 };
-
 var map = new kakao.maps.Map(container, options);
+
+var markerPositions = [
+  new kakao.maps.LatLng(35.88720165868838, 128.58509478903335), // Blue marker position
+  new kakao.maps.LatLng(35.88148754395694, 128.57626086398895), // Yellow marker position 1
+  new kakao.maps.LatLng(35.88082239685244, 128.59862890053688), // Yellow marker position 2
+];
+
+for (var i = 0; i < markerPositions.length; i++) {
+  var markerOptions = {
+    position: markerPositions[i],
+    map: map,
+    image: {
+      src: (i === 0) ? 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-3875d7.png' : 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-fdff5e.png',
+      size: new kakao.maps.Size(40, 40),
+      origin: new kakao.maps.Point(0, 0),
+      anchor: new kakao.maps.Point(20, 40)
+    }
+  };
+
+  var marker = new kakao.maps.Marker(markerOptions);
+}
